6044c0baef7eba2e611f855ce1815f10ecb81ebe
amos <amosjshinkle@gmail.com>
2022-11-23 11:10:04 -0500

    commit tree 89af2b172794a851d80b25361bda4b15cd643243

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 62
+let num2 = 1
 
-// Write your code here
+const multiply = num1*num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+random; 
+
+const num3 = 24
+const num4 = 5
+
+const mod = num3%num4
+
+const max = Math.max(20, 1, 3)
